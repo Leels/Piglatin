2c35846596474c3056967e2ed5cc0990c5ad91aa
Student <example@example.com>
2019-10-30 16:15:45 -0700

    add logic to convert words starting with consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 // Apple
 
 
@@ -5,7 +6,6 @@
 
 // THIS FUNCTION WILL TURN WORDS INTO PIG LATIN
 
-// var vowels = ["a", "e", "i", "o", "u"]
 // var letters = ["a", "p", "p", "l", "e"]
 
 
@@ -13,17 +13,23 @@ $(document).ready(function(){
   $("form#pig-latin").submit(function(event){
     event.preventDefault();
 
+    var vowels = ["a", "e", "i", "o", "u"]
     var originalString = $("input#phrase").val();
     var letters = originalString.split("");
 
 
-    for (var i = 0; i < letters.length; i ++) {
-      if (letters[0] === "a" || letters[0] ==="e" || letters[0] === "i" || letters[0] === "o" || letters[0] === "u") {
-        var newLetters = letters.splice(0,1);
-        letters = letters.join("") + newLetters + "way"
-        console.log(letters);
-      } else 
+    if (vowels.includes(letters[0])) {
+      var newLetters = letters.shift();
+      newLetters = letters.join("") + newLetters + "way"
+    } else {
+      for (var i = 0; i < letters.length; i ++) {
+        var cutConsonant = "";
+        if (!vowels.includes(letters[0])) {
+          cutConsonant = letters.shift()
+          letters.push(cutConsonant)
+        }
+      }
+        letters = letters.join("") + "ay"
     };
-
   });
 });
